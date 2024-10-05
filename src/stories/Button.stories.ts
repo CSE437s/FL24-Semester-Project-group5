b70f911a13124c021bc08abd6fa5fca48f38f613
adejolaO <aogunsan@cmitsouth.org>
2024-10-05 16:06:54 -0500

    messaging ui setup

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -51,10 +51,3 @@ export const Small: Story = {
     label: 'Button',
   },
 };
-
-export const Story1: Story = {
-  args: {
-    primary: false,
-    label: "Button"
-  }
-};
---
+++ src/stories/components/Messaging.stories.ts
@@ -0,0 +1,9 @@
+import type { Meta } from '@storybook/react';
+
+import { Messaging } from './Messaging.jsx';
+
+const meta: Meta<typeof TextBox> = {
+  component: TextBox,
+};
+
+export default meta;
