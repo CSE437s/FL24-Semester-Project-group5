a508d9b6a621e860ab9cab72ae551bc3c983e37b
adejolaO <aogunsan@cmitsouth.org>
2024-11-02 21:53:55 -0500

    commiting furniture and listing ui

--- src/app/furniture/[id]/page.tsx
+++ src/app/furniture/[id]/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { Card, CardContent, CardMedia, Typography, Box, Grid, Button, Paper } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, Box, Button, Paper, Grid, Rating, CircularProgress } from '@mui/material';
 import { useRouter, useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Maps from '../../components/map-card';
-import { getCoordinatesOfAddress } from '../../utils'; 
+import { getCoordinatesOfAddress } from '../../utils';
 
 // const theme = createTheme({
 //   typography: {
@@ -42,11 +42,11 @@ const FurnitureDescriptionPage = () => {
   const id = params['id'];
   const [locations, setLocations] = useState<Location[]>([]);
   const address = [''];
-  
+
 
   const [furnitureItem, setFurnitureItem] = useState<FurnitureItem | null>(null);
-  const [error, setError] = useState<string | null>(null); 
-  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (id) {
@@ -57,14 +57,14 @@ const FurnitureDescriptionPage = () => {
           if (response.ok) {
             if (data.location) {
               const coords = await getCoordinatesOfAddress(data.location);
-            if (coords) {
-              setLocations([{
-                latitude: coords.latitude,
-                longitude: coords.longitude,
-                description: data.description || "Furniture",
-              }]);
+              if (coords) {
+                setLocations([{
+                  latitude: coords.latitude,
+                  longitude: coords.longitude,
+                  description: data.description || "Furniture",
+                }]);
+              }
             }
-          }
             setFurnitureItem(data);
           } else {
             setError(`Error: ${response.status} - ${data.message}`);
@@ -72,109 +72,123 @@ const FurnitureDescriptionPage = () => {
         } catch (error) {
           setError('Error fetching furniture item: ' + error);
         } finally {
-          setLoading(false); 
+          setLoading(false);
         }
       };
       fetchFurnitureItem();
     }
   }, [id]);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>{error}</div>; 
-  if (!furnitureItem) return <div>No furniture item found.</div>; 
+  if (loading) return <div><Box sx={{ position:'absolute', top:'50%', left:'50%'}}>
+  <CircularProgress size='4rem'/>
+</Box></div>;
+  if (error) return <div>{error}</div>;
+  if (!furnitureItem) return <div>No furniture item found.</div>;
   address.push(furnitureItem?.location);
 
-  
-  const colorList = Array.isArray(furnitureItem.colors) 
-  ? furnitureItem.colors.join(', ') 
-  : 'None';
+
+  const colorList = Array.isArray(furnitureItem.colors)
+    ? furnitureItem.colors.join(', ')
+    : 'None';
 
   return (
     <Box sx={{ padding: '20px', maxWidth: '1200px', margin: '20px auto' }}>
-      <Card 
-        sx={{ 
-          boxShadow: 6, 
-          borderRadius: 2, 
-          minHeight: '80vh', 
-          display: 'flex', 
+      <Card
+        sx={{
+          boxShadow: 6,
+          borderRadius: 2,
+          minHeight: '80vh',
+          display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'center'
+          justifyContent: 'center',
+          padding: 4,
         }}
       >
-        <Paper elevation={10}>
         <CardMedia
           component="img"
           height="300"
           image={furnitureItem.pics[0] || "https://via.placeholder.com/400x300"}
           alt="Listing Image"
-          sx={{ objectFit: 'cover' }}
+          // sx={{ objectFit: 'cover' }}
+          sx={{ borderRadius: 2 }}
         />
-        </Paper>
         <CardContent>
 
           <Typography variant="h4" component="div" gutterBottom>
             {furnitureItem.description}
           </Typography>
+          <Grid item xs={9}>
+            <Typography variant="h6" color="text.secondary">
+              Rating:
+            </Typography>
+            {/* <Typography variant="body1">{furnitureItem.rating}</Typography> */}
+            <Rating name="furniture-rating" value={furnitureItem.rating} readOnly />
+          </Grid>
+          <br></br>
 
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
+
+          <Card 
+            sx={{padding:4, borderRadius:3, minWidth:300}}
+          >
+            <Grid item xs={6} 
+              sx={{display:'flex', flexDirection:'row', gap:1}}
+            >
               <Typography variant="h6" color="text.secondary">
                 Price:
               </Typography>
-              <Typography variant="body1">${furnitureItem.price}</Typography>
+              <Typography variant="h6">${furnitureItem.price}</Typography>
             </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h6" color="text.secondary">
-                Condition:
-              </Typography>
-              <Typography variant="body1">{furnitureItem.condition}</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h6" color="text.secondary">
-                Rating:
-              </Typography>
-              <Typography variant="body1">{furnitureItem.rating}</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h6" color="text.secondary">
-                Colors:
-              </Typography>
-              <Typography variant="body1">{colorList}</Typography>
-            </Grid>
-            <Grid item xs={6}>
-            {locations.length > 0 ? (
-          <Box sx={{  height: '200px', marginTop: '10px' }}>
-            <Maps locations={locations} names={address} />
-          </Box>
-        ) : (
-          <Typography variant="body1" color="text.secondary">
-            No pick-up location set
-          </Typography>
-        )}
-         </Grid>
+            <br></br>
+            <Grid container spacing={2}>
+              <Grid item xs={6} sx={{display:'flex', flexDirection:'row', gap:1}}>
+                <Typography variant="h6" color="text.secondary">
+                  Condition:
+                </Typography>
+                <Typography variant="h6">{furnitureItem.condition}</Typography>
+              </Grid>
 
-         <Grid item xs={6}>
-              <Typography variant="h6" color="text.secondary">
-                Seller:
-              </Typography>
-              <Button 
-  variant="contained" 
-  color="primary" 
-  onClick={() => router.push(`../profile?userId=${furnitureItem.user_id}`)}
->
-  View Profile
-</Button>
-            </Grid>
-          </Grid>
+              <Grid item xs={6} sx={{display:'flex', flexDirection:'row', gap:1}}>
+                <Typography variant="h6" color="text.secondary">
+                  Colors:
+                </Typography>
+                <Typography variant="h6">{colorList}</Typography>
+              </Grid>
 
-          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+        
+              <Grid item xs={6}>
+                {locations.length > 0 ? (
+                  <Box sx={{ height: '200px', marginTop: '10px' }}>
+                    <Maps locations={locations} names={address} />
+                  </Box>
+                ) : (
+                  <Typography variant="body1" color="text.secondary">
+                    No pick-up location set
+                  </Typography>
+                )}
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h6" color="text.secondary" sx={{paddingBottom:2}}>
+                  Seller:
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => router.push(`../profile?userId=${furnitureItem.user_id}`)}
+                >
+                  View Profile
+                </Button>
+              </Grid>
+              </Grid>
+          </Card>
+          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '10px', paddingTop:'1rem'}}>
             <Button variant="contained" color="primary" onClick={() => router.back()}>
               Back to Listings
             </Button>
           </Box>
         </CardContent>
       </Card>
-    </Box>
+    </Box >
   );
 };
 
